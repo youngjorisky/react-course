693b543f0ebbfbc4cb0fcb7240bf79a4e99fce6e
Young Jorisky <jojoyoung3@gmail.com>
2026-07-25 06:51:36 +0000

    Use data from the backend to generate the html

--- ecommerce-project/src/Pages/HomPage.jsx
+++ ecommerce-project/src/Pages/HomPage.jsx
@@ -1,13 +1,17 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 import checkmark from "../assets/images/icons/checkmark.png";
 
 export function HomePage() {
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    console.log(response.data);
-  });
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
 
   return (
     <>
