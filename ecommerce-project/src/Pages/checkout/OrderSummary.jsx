4e871d1106ddf20f5689c24129992658da861aee
Young Jorisky <jojoyoung3@gmail.com>
2026-07-30 20:19:38 +0000

    Orders page and orders summary works

--- ecommerce-project/src/Pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/Pages/checkout/OrderSummary.jsx
@@ -1,6 +1,5 @@
-import dayjs from "dayjs";
-import { formatMoney } from "../../utils/money";
-import { DeliveryOptions } from "./DeliveryOptions";
+import { CartItemDetails } from "./CartItemDetails";
+import { DeliveryDate } from "./DeliveryDate";
 
 export function OrderSummary({ cart, deliveryOptions }) {
   return (
@@ -15,38 +14,10 @@ export function OrderSummary({ cart, deliveryOptions }) {
 
           return (
             <div key={cartItem.productId} className="cart-item-container">
-              <div className="delivery-date">
-                Delivery date:
-                {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format(
-                  "dddd, MMMM D",
-                )}
-              </div>
+              <DeliveryDate selectedDeliveryOption={selectedDeliveryOption} />
 
               <div className="cart-item-details-grid">
-                <img className="product-image" src={cartItem.product.image} />
-
-                <div className="cart-item-details">
-                  <div className="product-name">{cartItem.product.name}</div>
-                  <div className="product-price">
-                    {formatMoney(cartItem.product.priceCents)}
-                  </div>
-                  <div className="product-quantity">
-                    <span>
-                      Quantity:{" "}
-                      <span className="quantity-label">
-                        {cartItem.quantity}
-                      </span>
-                    </span>
-                    <span className="update-quantity-link link-primary">
-                      Update
-                    </span>
-                    <span className="delete-quantity-link link-primary">
-                      Delete
-                    </span>
-                  </div>
-                </div>
-
-                <DeliveryOptions
+                <CartItemDetails
                   cartItem={cartItem}
                   deliveryOptions={deliveryOptions}
                 />
