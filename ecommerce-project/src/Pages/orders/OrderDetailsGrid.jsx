31072c2dd4a32ce5310d9086fa308c935d7fbe64
Young Jorisky <jojoyoung3@gmail.com>
2026-07-30 20:54:59 +0000

    order header and order details grid into own components

---
+++ ecommerce-project/src/Pages/orders/OrderDetailsGrid.jsx
@@ -0,0 +1,43 @@
+import { Fragment } from "react";
+import { Link } from "react-router";
+import dayjs from "dayjs";
+import buyAgainIcon from "../../assets/images/icons/buy-again.png";
+
+export function OrderDetailsGrid({ order }) {
+  return (
+    <div className="order-details-grid">
+      {order.products.map((orderProduct) => {
+        return (
+          <Fragment key={orderProduct.product.id}>
+            <div className="product-image-container">
+              <img src={orderProduct.product.image} />
+            </div>
+
+            <div className="product-details">
+              <div className="product-name">{orderProduct.product.name}</div>
+              <div className="product-delivery-date">
+                Arriving on:
+                {dayjs(orderProduct.estimatedDeliveryTimeMs).format("MMM D")}
+              </div>
+              <div className="product-quantity">
+                Quantity: {orderProduct.quantity}
+              </div>
+              <button className="buy-again-button button-primary">
+                <img className="buy-again-icon" src={buyAgainIcon} />
+                <span className="buy-again-message">Add to Cart</span>
+              </button>
+            </div>
+
+            <div className="product-actions">
+              <Link to="/tracking">
+                <button className="track-package-button button-secondary">
+                  Track package
+                </button>
+              </Link>
+            </div>
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
---
+++ ecommerce-project/src/Pages/orders/OrdersGrid.jsx
@@ -0,0 +1,18 @@
+import { OrderHeader } from "./OrderHeader";
+import { OrderDetailsGrid } from "./OrderDetailsGrid";
+
+export function OrdersGrid({ orders }) {
+  return (
+    <div className="orders-grid">
+      {orders.map((order) => {
+        return (
+          <div key={order.id} className="order-container">
+            <OrderHeader order={order} />
+
+            <OrderDetailsGrid order={order} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
