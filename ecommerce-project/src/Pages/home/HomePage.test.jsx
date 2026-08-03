687166c7345c1bef15fb568136149d07d835f3fc
Young Jorisky <jojoyoung3@gmail.com>
2026-08-03 17:14:22 +0000

    deleted the userEvent for HomePage.text.jsx

--- ecommerce-project/src/Pages/home/HomePage.test.jsx
+++ ecommerce-project/src/Pages/home/HomePage.test.jsx
@@ -1,7 +1,6 @@
 import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen, within } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
-import userEvent from "@testing-library/user-event";
 import axios from "axios";
 import { HomePage } from "./HomePage";
 
@@ -63,9 +62,7 @@ describe("HomePage component", () => {
     ).toBeInTheDocument();
 
     expect(
-      within(productContainers[1]).getByText(
-        "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      ),
+      within(productContainers[1]).getByText("Intermediate Size Basketball"),
     ).toBeInTheDocument();
   });
 });
