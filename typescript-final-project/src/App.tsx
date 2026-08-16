0929cc5732b7229ec30d40ea5196849b3bd95501
Young Jorisky <jojoyoung3@gmail.com>
2026-08-16 19:15:44 +0000

    Addition of products and update button of products

--- typescript-final-project/src/App.tsx
+++ typescript-final-project/src/App.tsx
@@ -1,122 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import type { Product } from "./types";
+import { products as initialProductshow } from "./data/product";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState<Product[]>(initialProductshow);
+  const [productName, setProductName] = useState<string>("");
+  const [price, setPrice] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+
+  function addProduct(product: Product) {
+    setProducts([...products, product]);
+  }
+
+  function typingName(event: React.ChangeEvent<HTMLInputElement>) {
+    setProductName(event.target.value);
+  }
+
+  function typingPrice(event: React.ChangeEvent<HTMLInputElement>) {
+    setPrice(event.target.value);
+  }
+
+  function typingCategory(event: React.ChangeEvent<HTMLInputElement>) {
+    setCategory(event.target.value);
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h1>Products</h1>
 
-      <div className="ticks"></div>
+      <input
+        value={productName}
+        onChange={typingName}
+        placeholder="Input product name"
+      />
+      <input
+        value={price}
+        onChange={typingPrice}
+        placeholder="Input product price"
+      />
+      <input
+        value={category}
+        onChange={typingCategory}
+        placeholder="Input product category"
+      />
+      <button
+        onClick={() => {
+          const newProduct: Product = {
+            id: crypto.randomUUID(),
+            name: productName,
+            price: Number(price),
+            category: category,
+            inStock: true,
+          };
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+          addProduct(newProduct);
+        }}
+      >
+        Add Product
+      </button>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>${product.price}</p>
+          <p>{product.category}</p>
+          <p>{product.inStock ? "In Stock" : "Out of Stock"}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
