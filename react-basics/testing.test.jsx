430dff40654fe7464962459bc7c0727b778fae10
Jojo  Young <jojoyoung3@gmail.com>
2026-08-21 15:10:55 +0000

    Delete react-basics directory

--- react-basics/testing.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
-import { Alert } from "./alert";
-
-test("Alert Message", () => {
-  render(<Alert message={"Something went wrong"} />);
-  const alertMessage = screen.getByRole("alert");
-  expect(alertMessage).toBeInTheDocument();
-});
